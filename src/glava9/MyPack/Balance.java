75e5bd1f268913e92998bc1192d07e8569feda66
vipeople <vipeople@yandex.ru>
2018-09-12 17:12:02 +0300

    Add new class (пакеты и интерфейсы)

---
+++ src/glava9/MyPack/Balance.java
@@ -0,0 +1,23 @@
+// Простой пакет
+package glava9.MyPack;
+
+/**
+ * Created by prilipko.ia on 12.09.2018.
+ */
+public class Balance {
+    String name;
+    double bal;
+
+    Balance(String n, double b){
+        name = n;
+        bal = b;
+    }
+
+    void show() {
+        if(bal < 0) {
+            System.out.print("--> ");
+            System.out.println(name + ": $" + bal);
+        }
+    }
+}
+
